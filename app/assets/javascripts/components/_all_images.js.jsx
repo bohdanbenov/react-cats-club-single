b4088f110b92f156729cc17176f0585867894afc
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-18 22:06:15 +0300

    Add componentDidMount for geting all data about images and save it via setState.

--- app/assets/javascripts/components/_all_images.js.jsx
+++ app/assets/javascripts/components/_all_images.js.jsx
@@ -1,8 +1,29 @@
 class AllImages extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: []
+        };
+    }
+
+    componentDidMount(){
+        fetch('/api/v1/images.json')
+            .then((response) => {return response.json()})
+            .then((data) => {this.setState({ images: data }) });
+    }
+
     render(){
+        var fruits = this.state.fruits.map((fruit) => {
+            return(
+                <div key={fruit.id}>
+                    <h1>{fruit.name}</h1>
+                    <p>{fruit.description}</p>
+                </div>
+            )
+        })
         return(
             <div>
-                <h1>To do: List of fruits</h1>
+                {fruits}
             </div>
         )
     }
