6eebfc2eda44a008e6fbb45bf1878ae864c2ee28
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-18 17:42:39 +0300

    Add seeds file for filling database with images url.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+require 'net/http'
+require 'uri'
+
+20.times do |x|
+  faker = Faker::LoremFlickr.image
+  first_part = faker.slice"http://loremflickr.com"
+
+  r = Net::HTTP.get_response(URI.parse(faker))
+  if r.code == "301"
+    r = Net::HTTP.get_response(URI.parse(r.header['location']))
+    second_part = r.header['location']
+  end
+
+  url = first_part + second_part
+  Image.create!(url: url)
+  puts "URL #{x} was added to database!"
+end
