89854df2d3dfcd742f1c70c154899c8762fa12e2
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-18 23:18:42 +0300

    Added all handles fuctions.

--- app/assets/javascripts/components/_all_images.js.jsx
+++ app/assets/javascripts/components/_all_images.js.jsx
@@ -2,7 +2,7 @@ const AllImages = (props) => {
     let images = props.images.map((image) => {
         return(
             <div key={image.id}>
-                <Image image={image}/>
+                <Image image={image} handleDelete={props.handleDelete} handleUpdate={props.handleUpdate}/>
             </div>
         )
     })
