d0259a17c6c513eace0ef6db9e0dc6af46847f3d
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-18 22:37:59 +0300

    Added handle for submit button, and addNewImage method.

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -4,7 +4,30 @@ class Body extends React.Component {
         this.state = {
             images: []
         };
+        this.handleFormSubmit = this.handleFormSubmit.bind(this)
+        this.addNewImage = this.addNewImage.bind(this)
     }
+
+    handleFormSubmit(url){
+        let body = JSON.stringify({image: {url: url}})
+        fetch('http://localhost:3000/api/v1/images', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: body,
+        }).then((response) => {return response.json()})
+            .then((image)=>{
+                this.addNewImage(image)
+            })
+
+    }
+    addNewImage(image){
+        this.setState({
+            images: this.state.images.concat(image)
+        })
+    }
+
     componentDidMount(){
         fetch('/api/v1/images.json')
             .then((response) => {return response.json()})
@@ -13,6 +36,7 @@ class Body extends React.Component {
     render(){
         return(
             <div>
+                <NewImage handleFormSubmit={this.handleFormSubmit}/>
                 <AllImages images={this.state.images} />
             </div>
         )
