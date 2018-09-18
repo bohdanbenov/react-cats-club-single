d41711939f58bcb13ed7467b6b0d7d2938c2ba88
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-18 22:45:12 +0300

    Used Image component.

--- app/assets/javascripts/components/_all_images.js.jsx
+++ app/assets/javascripts/components/_all_images.js.jsx
@@ -2,7 +2,7 @@ const AllImages = (props) => {
     let images = props.images.map((image) => {
         return(
             <div key={image.id}>
-                <p>{image.url}</p>
+                <Image image={image}/>
             </div>
         )
     })
