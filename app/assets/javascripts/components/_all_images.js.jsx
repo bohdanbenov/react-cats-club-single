ac43500b796aef278270b697995f393144dcb08a
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-18 22:37:09 +0300

    Change content of tag p for correct display of image url.

--- app/assets/javascripts/components/_all_images.js.jsx
+++ app/assets/javascripts/components/_all_images.js.jsx
@@ -2,7 +2,7 @@ const AllImages = (props) => {
     let images = props.images.map((image) => {
         return(
             <div key={image.id}>
-                <p>{image.description}</p>
+                <p>{image.url}</p>
             </div>
         )
     })
