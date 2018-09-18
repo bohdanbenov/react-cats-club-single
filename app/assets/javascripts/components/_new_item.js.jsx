33686661d931e1732d08e4f3a89e29588616d53f
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-18 22:39:17 +0300

    Added onSubmit action.

---
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -0,0 +1,12 @@
+const NewImage = (props) => {
+    let formFields = {}
+
+    return(
+        <form onSubmit={(e) => {props.handleFormSubmit(formFields.url.value);
+                                e.target.reset();}
+        }>
+            <input ref={input => formFields.url = input} placeholder='Enter url of the picture'/>
+            <button>Submit</button>
+        </form>
+    )
+}
