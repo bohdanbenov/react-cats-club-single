5078a1e2e46ce93e6416acc29d0af40c87a29cf8
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-18 23:19:48 +0300

    Added edit button and handler for it.

--- app/assets/javascripts/components/_image.js.jsx
+++ app/assets/javascripts/components/_image.js.jsx
@@ -1,9 +1,31 @@
 class Image extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            editable: false
+        }
+        this.handleEdit = this.handleEdit.bind(this)
+    }
+
+    handleEdit(){
+        if(this.state.editable){
+            let url = this.url.value
+            let id = this.props.image.id
+            let image = {id: id, url: url}
+            this.props.handleUpdate(image)
+        }
+        this.setState({
+            editable: !this.state.editable
+        })
+    }
 
     render(){
+        let url = this.state.editable ? <input type='text' ref={input => this.url = input} defaultValue={this.props.image.url}/>:<p>{this.props.image.url}</p>
         return(
             <div>
-                <p>{this.props.image.url}</p>
+                {url}
+                <button onClick={() => this.handleEdit()}>{this.state.editable? 'Submit' : 'Edit'}</button>
+                <button onClick={() => this.props.handleDelete(this.props.image.id)}>Delete</button>
             </div>
         )
     }
